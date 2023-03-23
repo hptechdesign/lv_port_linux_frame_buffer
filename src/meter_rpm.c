3c35a5ed411c74bdb8b642da39ca1664b0a408b8
Huw Price <huw@hptechdesign.uk>
2023-03-23 17:04:06 +0000

    no magic numbers!

--- src/meter_rpm.c
+++ src/meter_rpm.c
@@ -1,5 +1,6 @@
 #include "lvgl/lvgl.h"
 #include "meter_rpm.h"
+#include <stdio.h>
 
 static lv_obj_t * meter;
 
@@ -19,7 +20,7 @@ void meter_rpm(void)
 
     /*Add a scale first*/
     lv_meter_scale_t * scale = lv_meter_add_scale(meter);
-    lv_meter_set_scale_range(meter, scale, 0, 3000, 270, 135);
+    lv_meter_set_scale_range(meter, scale, METER_RPM_MIN, METER_RPM_MAX, 270, 135);
     lv_meter_set_scale_ticks(meter, scale, 41, 2, 10, lv_palette_main(LV_PALETTE_GREY));
     lv_meter_set_scale_major_ticks(meter, scale, 8, 4, 15, lv_color_black(), 15);
 
@@ -27,35 +28,44 @@ void meter_rpm(void)
 
     /*Add a blue arc to the start*/
     indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_BLUE), 0);
-    lv_meter_set_indicator_start_value(meter, indic, 0);
-    lv_meter_set_indicator_end_value(meter, indic, 600);
+    lv_meter_set_indicator_start_value(meter, indic, METER_RPM_MIN);
+    lv_meter_set_indicator_end_value(meter, indic, METER_RPM_LOW_THRESH);
 
     /*Make the tick lines blue at the start of the scale*/
     indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_BLUE),
                                      false, 0);
-    lv_meter_set_indicator_start_value(meter, indic, 0);
-    lv_meter_set_indicator_end_value(meter, indic, 600);
+    lv_meter_set_indicator_start_value(meter, indic, METER_RPM_MIN);
+    lv_meter_set_indicator_end_value(meter, indic, METER_RPM_LOW_THRESH);
 
     /*Add a red arc to the end*/
     indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_RED), 0);
-    lv_meter_set_indicator_start_value(meter, indic, 2400);
-    lv_meter_set_indicator_end_value(meter, indic, 3000);
+    lv_meter_set_indicator_start_value(meter, indic, METER_RPM_HIGH_THRESH);
+    lv_meter_set_indicator_end_value(meter, indic, METER_RPM_MAX);
 
     /*Make the tick lines red at the end of the scale*/
     indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED), false,
                                      0);
-    lv_meter_set_indicator_start_value(meter, indic, 2400);
-    lv_meter_set_indicator_end_value(meter, indic, 3000);
+    lv_meter_set_indicator_start_value(meter, indic, METER_RPM_HIGH_THRESH);
+    lv_meter_set_indicator_end_value(meter, indic, METER_RPM_MAX);
 
     /*Add a needle line indicator*/
     indic = lv_meter_add_needle_line(meter, scale, 4, lv_palette_main(LV_PALETTE_GREY), -10);
+
+    // Draw label
+    lv_obj_t * label1 = lv_label_create(lv_scr_act());
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_WRAP);     /*Break the long lines*/
+    lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
+    lv_label_set_text(label1, "#ffffff RPM, rev/min");
+    lv_obj_set_width(label1, METER_RPM_SIZE);  /*Set smaller width to make the lines wrap*/
+    lv_obj_set_style_text_align(label1, LV_TEXT_ALIGN_CENTER, 0);
+    lv_obj_set_pos(label1, METER_RPM_XPOS, METER_RPM_YPOS+METER_RPM_SIZE-45);
 
     /*Create an animation to set the value*/
     lv_anim_t a;
     lv_anim_init(&a);
     lv_anim_set_exec_cb(&a, set_value);
     lv_anim_set_var(&a, indic);
-    lv_anim_set_values(&a, 0, 2400);
+    lv_anim_set_values(&a, METER_RPM_MIN, METER_RPM_HIGH_THRESH);
     lv_anim_set_time(&a, 2000);
     lv_anim_set_repeat_delay(&a, 100);
     lv_anim_set_playback_time(&a, 500);
--- src/meter_rpm.h
+++ src/meter_rpm.h
@@ -1,6 +1,12 @@
 #define METER_RPM_XPOS 520
-#define METER_RPM_YPOS 200
+#define METER_RPM_YPOS 230
 #define METER_RPM_SIZE 250
+
+#define METER_RPM_MIN 0
+#define METER_RPM_MAX 3000
+
+#define METER_RPM_LOW_THRESH  (METER_RPM_MAX*0.2)
+#define METER_RPM_HIGH_THRESH  (METER_RPM_MAX*0.8)
 
 
 void meter_rpm(void);
