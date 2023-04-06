239b15e2182947d32e25b8788f2077344ccfac00
huw <huw.price@excelitas.com>
2023-04-06 05:07:14 +0100

    fix compile warnings

--- src/sensor.c
+++ src/sensor.c
@@ -72,22 +72,22 @@ int sensor_spoofNext(int current, int target, int maxPercentDiff)
 void sensor_generateData(void)
 {
 
-    sensor_setCrankRpm(sensor_spoofNext( //
+    sensor_setCrankRpm(sensor_spoofNext(\
         sensor_getCrankRpm(), SPOOF_CRANK_RPM_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 
-    sensor_setManifoldPressure(sensor_spoofNext( //
+    sensor_setManifoldPressure(sensor_spoofNext(\
         sensor_getManifoldPressure(), SPOOF_MANIFOLD_PRESSURE_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 
-    sensor_setTemperatureA(sensor_spoofNext( //
+    sensor_setTemperatureA(sensor_spoofNext(\
         sensor_getTemperatureA(), SPOOF_TEMPERATURE_A_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 
-    sensor_setTemperatureB(sensor_spoofNext( //
+    sensor_setTemperatureB(sensor_spoofNext(\
         sensor_getTemperatureB(), SPOOF_TEMPERATURE_B_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 
-    sensor_setOilPressure(sensor_spoofNext( //
+    sensor_setOilPressure(sensor_spoofNext(\
         sensor_getOilPressure(), SPOOF_OIL_PRESSURE_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 
-    sensor_setFuelPressure(sensor_spoofNext( //
+    sensor_setFuelPressure(sensor_spoofNext(\
         sensor_getFuelPressure(), SPOOF_FUEL_PRESSURE_MEDIAN, SPOOF_MAX_STEP_PERCENT));
 }
 
@@ -100,12 +100,12 @@ void sensor_generateData(void)
  */
 void sensor_fillBufWithCurrentData(char * buf)
 {
-    snprintf(buf, 512, "Payload sent:\n \\
-        Crank_rpm              = %4d\n \\
-        Manifold_pressure_mbar = %4d\n \\
-        Temperature_a_degC     = %4d\n \\
-        Temperature_b_degC     =  %4d\n \\
-        Oil_pressure_mbar      = %4d\n \\
+    snprintf(buf, 512, "Payload sent:\n\
+        Crank_rpm              = %4d\n\
+        Manifold_pressure_mbar = %4d\n\
+        Temperature_a_degC     = %4d\n\
+        Temperature_b_degC     =  %4d\n\
+        Oil_pressure_mbar      = %4d\n\
         Fuel_pressure_bar      = %4d\n",
              sensor_data.crank_rpm, sensor_data.manifold_pressure_mbar, sensor_data.temperature_a_degC,
              sensor_data.temperature_b_degC, sensor_data.oil_pressure_mbar, sensor_data.fuel_pressure_bar);
