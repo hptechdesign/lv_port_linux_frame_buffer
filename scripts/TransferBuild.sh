a679498b38fd8484e549de68e35b8643e96c3acf
huw <huw.price@excelitas.com>
2023-04-06 05:45:52 +0100

    fix launch targets and transfer to /bin on RPI

--- scripts/TransferBuild.sh
+++ scripts/TransferBuild.sh
@@ -12,8 +12,10 @@ SFTP_PWD=$RPI_PASSWORD
 echo "Issuing pkill gdbserver command to tidy up Raspberry PI..."
 sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'pkill gdbserver'
 echo "Transferring rpi_ecu_display to Raspberry PI..."
-lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER  -e "put ./build/rpi_ecu_display; bye"
+sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'mkdir -p rpi_ecu_display/bin; chmod 771 rpi_ecu_display/bin'
+sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'mkdir -p rpi_ecu_display/log; chmod 771 rpi_ecu_display/log'
+lftp sftp://$SFTP_USER:$SFTP_PWD@$SFTP_SERVER  -e "put -O rpi_ecu_display/bin bin/rpi_ecu_display; bye"
 echo "Starting GdbServer on Raspberry PI"
-sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'chmod 771 ./rpi_ecu_display'
-sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'gdbserver localhost:9999 ./rpi_ecu_display'
+sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'chmod 771 rpi_ecu_display/bin/rpi_ecu_display'
+sshpass -p $RPI_PASSWORD ssh $RPI_USER@$RPI_IP 'gdbserver localhost:9999 rpi_ecu_display/bin/rpi_ecu_display'
 echo "GdbServer on Raspberry PI stopped, ready for next session."
