8149aac61116afb20ac59fb72b29a43202774238
Huw Price <huw@hptechdesign.uk>
2023-04-17 06:21:10 +0100

    fix typo

--- src/copy_release_bins.cmake
+++ src/copy_release_bins.cmake
@@ -4,11 +4,11 @@ if(${TARGET_PLATFORM} STREQUAL "win")
 	if(NOT CLEAN_FLAG STREQUAL "+")
 		message("Build is clean")
 		# Set release folder name
-		set(DEST ${CMAKE_SOURCE_DIR}/bin/x86_64_w64_${MAJ_VER}.${MIN_VER})
+		set(DEST ${CMAKE_SOURCE_DIR}/bin/x86_64_w64_v${MAJ_VER}.${MIN_VER})
 		add_custom_command(TARGET ${PROJECT_NAME} 
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${PROJECT_NAME}> 
-				   ${CMAKE_SOURCE_DIR}/bin/release_${MAJ_VER}.${MIN_VER}/${PROJECT_NAME}_${TARGET_SUFFIX}.exe)
+				   ${DEST}/${PROJECT_NAME}_${TARGET_SUFFIX}.exe)
 		add_custom_command(TARGET ecu_sensor_spoofer 
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:ecu_sensor_spoofer> 
@@ -24,7 +24,7 @@ else() # RPi_ecu_build
 	if(NOT CLEAN_FLAG STREQUAL "+")
 		message("Build is clean")
 		# Set release folder name
-		set(DEST ${CMAKE_SOURCE_DIR}/bin/armv8_rpi_raspbian_${MAJ_VER}.${MIN_VER})
+		set(DEST ${CMAKE_SOURCE_DIR}/bin/armv8_rpi_raspbian_v${MAJ_VER}.${MIN_VER})
 		add_custom_command(TARGET ${PROJECT_NAME} 
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${PROJECT_NAME}> 
