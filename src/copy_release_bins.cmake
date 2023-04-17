6f75f163d63fd917e2f298196662c53cb0e218ab
Huw Price <huw@hptechdesign.uk>
2023-04-17 05:37:34 +0100

    fix clean flag check in conditonal statement

--- src/copy_release_bins.cmake
+++ src/copy_release_bins.cmake
@@ -1,7 +1,8 @@
 # If a clean commit, copy the binaries and stamp with version details
 if(${TARGET_PLATFORM} STREQUAL "win")
-	
-	if(${CLEAN_FLAG} STREQUAL "")
+	message("Checking clean flag status")
+	if(NOT ${CLEAN_FLAG} STREQUAL "+")
+		message("Build is clean")
 		add_custom_command(TARGET ${PROJECT_NAME} 
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${PROJECT_NAME}> 
@@ -10,16 +11,20 @@ if(${TARGET_PLATFORM} STREQUAL "win")
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:ecu_sensor_spoofer> 
 				   ${CMAKE_SOURCE_DIR}/bin/release_${MAJ_VER}_${MIN_VER}/ecu_sensor_spoofer_${TARGET_SUFFIX}.exe)
+	else()
+		 message("Build is not clean")
 	endif()
 
 else() # RPi_ecu_build
-
-	if(${CLEAN_FLAG} STREQUAL "")
-
+	message("Checking clean flag status")
+	if(NOT ${CLEAN_FLAG} STREQUAL "+")
+	message("Build is clean")
 		add_custom_command(TARGET ${PROJECT_NAME} 
                    POST_BUILD
                    COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${PROJECT_NAME}> 
 				   ${CMAKE_SOURCE_DIR}/bin/release_${MAJ_VER}_${MIN_VER}/${PROJECT_NAME}_${TARGET_SUFFIX})
+	else()
+	message("Build is not clean")
 	endif()
 	
 endif()
